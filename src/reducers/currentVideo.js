9aa9254eb1a2a721c1cd13e4371cf8538a89cdd3
stellaliang21 <stellaliang2014@gmail.com>
2019-03-06 20:38:17 -0800

    Implement reducer and root reducer

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,20 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  
+  // switch(action.type) {
+  //   case 'CHANGE_VIDEO':
+  //   return action.video
+  //   default:
+  //   return state
+  // }
+
+  if(action.type === 'CHANGE_VIDEO') {
+    return action.video;
+  } else {
+    return state;
+  }
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,19 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
-  //TODO: define a reducer for the videoList field of our state.
+var videoListReducer = (state = [], action) => {
+  // TODO: define a reducer for the videoList field of our state.
+  // switch(action.type) {
+  //   case 'CHANGE_VIDEO_LIST':
+  //   return action.videos
+  //   default:
+  //   return state
+  // }
+
+  if (action.type === 'CHANGE_VIDEO_LIST') {
+    return action.videos;
+  } else {
+    return state;
+  }
 };
 
 export default videoListReducer;
